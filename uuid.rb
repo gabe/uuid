4a2b5b482a3333909dece2fe79731f6d22117325
Gabriel Boyer <gabe@hobo.local>
2008-08-14 09:56:57 -0600

    Added support for Win32 (required using the Win32 API for crypto-quality RNG, since Windows has no /dev/urandom or the like

--- uuid.rb
+++ uuid.rb
@@ -42,23 +42,67 @@ class UUID
   def to_urn
     "urn:uuid:#{self}"
   end
-  
-  def self.parse(value)
-    raise NotImplementedError
-  end
 end
 
 class UUID::Bytes < BitStruct
-  unsigned :time_low, 32
-  unsigned :time_mid, 16
-  unsigned :time_hi_and_version, 16
-  unsigned :clock_seq, 16
-  unsigned :node, 48
+  unsigned :time_low,             32  
+  unsigned :time_mid,             16
+  unsigned :time_hi_and_version,  16
+  unsigned :clock_seq,            16
+  unsigned :node,                 48
 end
 
 class UUID::RandomDevice
-  def read
-    File.read('/dev/urandom', 16)
+  if RUBY_PLATFORM =~ /win/i
+    
+    require 'Win32API'
+    
+    PROV_RSA_FULL                  = 1
+    CRYPT_VERIFYCONTEXT            = 0xF0000000
+    FORMAT_MESSAGE_IGNORE_INSERTS  = 0x00000200
+    FORMAT_MESSAGE_FROM_SYSTEM     = 0x00001000
+    
+    CryptAcquireContext  = Win32API.new("advapi32", "CryptAcquireContext", 'PPPII', 'L')
+    CryptGenRandom       = Win32API.new("advapi32", "CryptGenRandom", 'LIP', 'L')
+    CryptReleaseContext  = Win32API.new("advapi32", "CryptReleaseContext", 'LI', 'L')
+    GetLastError         = Win32API.new("kernel32", "GetLastError", '', 'L')
+    FormatMessageA       = Win32API.new("kernel32", "FormatMessageA", 'LPLLPLPPPPPPPP', 'L')
+    
+    def read
+      hProvStr = ' ' * 4
+      if CryptAcquireContext.call(hProvStr, nil, nil, PROV_RSA_FULL, CRYPT_VERIFYCONTEXT) == 0
+        raise SystemCallError, "CryptAcquireContext failed: #{last_error_message}"
+      end
+      hProv, _ = hProvStr.unpack('L')
+      bytes = ' ' * 16
+      if CryptGenRandom.call(hProv, 16, bytes) == 0
+        raise SystemCallError, "CryptGenRandom failed: #{last_error_message}"
+      end
+      if CryptReleaseContext.call(hProv, 0) == 0
+        raise SystemCallError, "CryptReleaseContext failed: #{last_error_message}"
+      end
+      bytes
+    end
+    
+    private
+    
+    def last_error_message
+      code = GetLastError.call
+      message = "\0" * 1024
+      length = FormatMessageA.call(FORMAT_MESSAGE_IGNORE_INSERTS + FORMAT_MESSAGE_FROM_SYSTEM, 0, code, 0, msg, 1024, nil, nil, nil, nil, nil, nil, nil, nil)
+      message[0, length].tr("\r", '').chomp
+    end
+    
+  else
+    
+    def read
+      random_device = ['/dev/urandom', '/dev/random'].find { |device| File.readable?(device) }
+      unless random_device
+        raise RuntimeError, 'Unable to find readable random device (e.g., /dev/urandom)'
+      end
+      File.read(random_device, 16)
+    end
+    
   end
 end
 
