661d4599ed3e156c56176914cff4a0f81a496e91
Gabriel Boyer <gabe@hobo.local>
2008-08-15 08:21:41 -0600

    Added extra specs

--- spec/random_device_spec.rb
+++ spec/random_device_spec.rb
@@ -8,4 +8,8 @@ describe UUID::RandomDevice, '#read' do
   it 'returns 16 bytes' do
     @device.read.size.should == 16
   end
+  
+  it 'returns different bytes for subsequent calls' do
+    @device.read.should_not == @device.read
+  end
 end
