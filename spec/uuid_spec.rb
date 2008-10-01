782441cdaa247b66e27a5d885f97997fd814004c
Gabriel Boyer <gboyer@gmail.com>
2008-09-30 19:18:09 -0600

    Fixed specs broken by previous change

--- spec/uuid_spec.rb
+++ spec/uuid_spec.rb
@@ -3,7 +3,7 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 describe UUID, '.uuid4' do
   it 'generates a new UUID' do
     UUID.uuid4.should be_a_kind_of(UUID)
-    UUID.uuid4.should_not == UUID.new
+    UUID.uuid4.should_not == UUID.uuid4
   end
   
   it 'generates version 4 UUIDs' do
@@ -42,7 +42,7 @@ describe UUID, '#initialize' do
   end
 
   it 'raises a RangeError when passed an Integer that is out of range for a UUID' do
-    lambda { UUID.new -1 }.should raise_error(RangeError)
+    lambda { UUID.new(-1) }.should raise_error(RangeError)
     lambda { UUID.new(1 << 128) }.should raise_error(RangeError)
   end
 end
@@ -60,19 +60,19 @@ delimited_hex_format = /[0-9a-fA-F]{8}(-[0-9a-fA-F]{4}){3}-[0-9a-fA-F]{12}/
 
 describe UUID, '#to_s' do
   it 'returns a UUID string in delimited hex format (i.e., xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx)' do
-    UUID.new.to_s.should =~ /^#{delimited_hex_format}$/
+    UUID.uuid4.to_s.should =~ /^#{delimited_hex_format}$/
   end
 end
 
 describe UUID, '#hex' do
   it 'returns a UUID string in undelimited hex format (i.e., xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx)' do
-    UUID.new.hex.should =~ /^[0-9a-fA-F]{8}([0-9a-fA-F]{4}){3}[0-9a-fA-F]{12}$/
+    UUID.uuid4.hex.should =~ /^[0-9a-fA-F]{8}([0-9a-fA-F]{4}){3}[0-9a-fA-F]{12}$/
   end
 end
 
 describe UUID, '#urn' do
   it 'returns a UUID string in delimited hex format with a urn prefix (i.e., urn:uuid:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx)' do
-    UUID.new.urn.should =~ /^urn:uuid:#{delimited_hex_format}$/
+    UUID.uuid4.urn.should =~ /^urn:uuid:#{delimited_hex_format}$/
   end
 end
 
